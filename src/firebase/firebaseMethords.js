46f59e437350739b2d72a66754f3643b73ed37fd
sameersiddique987 <sameersiddique301@gmail.com>
2024-11-27 19:16:41 +0500

    Updated file

--- src/firebase/firebaseMethords.js
+++ src/firebase/firebaseMethords.js
@@ -1,4 +1,6 @@
 
+import app from "./firebaseconfig";
+
 import {
     getAuth,
     createUserWithEmailAndPassword,
--- src/main.jsx
+++ src/main.jsx
@@ -4,16 +4,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Layout from './Layout.jsx'
-
 import Login from './pages/Login.jsx'
-
 import Dashboard from './pages/Dashboard.jsx'
 import Signup from './pages/Signup.jsx'
 import ProtectedRouts from './conponents/ProtectedRouts.jsx'
 import Profile from './pages/Profile.jsx'
 import AllBlogs from './pages/AllBlogs.jsx'
 
-import { Provider } from 'react-redux'
+
 
 const router = createBrowserRouter ([
   {
