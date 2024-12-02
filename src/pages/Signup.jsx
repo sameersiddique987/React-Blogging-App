06c3bb258b77bc9b639e8d81e4b8677b48573fce
sameersiddique987 <sameersiddique301@gmail.com>
2024-12-02 20:03:05 +0500

    updated file

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { useForm } from 'react-hook-form';
 
 import { signUpUser, uploadImage } from '../firebase/firebaseMethords';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 const Signup =  () => {
   const navigate = useNavigate()
   const {
@@ -71,6 +71,8 @@ const formValue = async (data) => {
   </div>
 
   <button  type="submit" className="text-white  bg-gradient-to-r from-lime-200 bg-[rgb(74,222,128)] hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-lime-300 dark:focus:ring-lime-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">SignUp</button>
+<br />
+<Link to="/Login">Please Login!</Link>
 </form>
 </div>
     </div>
