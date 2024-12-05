728f58b15c12b8d0f83778e13d516c60e884e5d3
sameersiddique987 <sameersiddique301@gmail.com>
2024-12-05 11:06:37 +0500

    Updated file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,20 @@
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: [
+//     "./index.html",
+//     "./src/**/*.{js,ts,jsx,tsx}",
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [
+//     require('daisyui'),
+//   ],
+// }
+
 /** @type {import('tailwindcss').Config} */
+import daisyui from 'daisyui';
+
 export default {
   content: [
     "./index.html",
@@ -8,7 +24,6 @@ export default {
     extend: {},
   },
   plugins: [
-    require('daisyui'),
+    daisyui,
   ],
 }
-
